c581624cb9982285c15c91a804bc9a95f481034b
Geverson <geversonjosedesouza@gmail.com>
2022-12-08 21:37:04 -0300

    Refatorando Classes de tratamento de exceção e cadastro de usuário

--- model/ResponseDefaultDTO.cs
+++ model/ResponseDefaultDTO.cs
@@ -1,7 +1,7 @@
 ﻿/// <summary>
 /// Classe que representa validação do token
 /// </summary>
-public class TokenValidado
+public class ResponseDefaultDTO
 {
     public int StatusCode { get; set; }
     public string Mensagem { get; set; }
--- model/UsuarioLoginRequest.cs
+++ model/UsuarioLoginRequest.cs
@@ -1,4 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Newtonsoft.Json;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
 
 namespace AppClassLibraryClient.model
 {
@@ -9,6 +12,10 @@ namespace AppClassLibraryClient.model
         /// </summary>
         /// <example>user0@email.com</example>
         [Required]
+        [DataMember(Name = "email")]
+        [JsonProperty(PropertyName = "email")]
+        [StringLength(2, ErrorMessage = "A quantidade máxima de caracteres é 255. ")]
+        [DefaultValue("")]
         public string Email { get; set; }
 
         /// <summary>
@@ -16,6 +23,10 @@ namespace AppClassLibraryClient.model
         /// </summary>
         /// <example>q1w2e3r4</example>
         [Required]
+        [DataMember(Name = "senha")]
+        [JsonProperty(PropertyName = "senha")]
+        [StringLength(255, ErrorMessage = "A quantidade máxima de caracteres é 255. ")]
+        [DefaultValue("")]
         public string Senha { get; set; }
     }
 }
